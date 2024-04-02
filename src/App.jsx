00660da7285466be4a0b30587a0d8a054a6805fa
jerickmalungcut <jerickmalungcut@yahoo.com>
2024-04-02 23:38:17 +0800

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Feature from "./components/Feature";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Workflow from "./components/Workflow";
+
 function App() {
   return (
     <>
-      <div>Hello Jerick!</div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+        <Feature />
+        <Workflow />
+        <Pricing />
+      </div>
     </>
   );
 }
